c013f3c3db00f28f4a0fbe473ca40b899341aa30
Alexander Pevzner <pzz@apevzner.com>
2019-11-25 23:00:05 +0300

    Cosmetic

--- airscan.h
+++ airscan.h
@@ -619,7 +619,7 @@ typedef struct {
     SANE_Word    min_hei_mm, max_hei_mm;   /* Min/max height, in millimeters */
     SANE_Word    *resolutions;             /* Discrete resolutions, in DPI */
     SANE_Range   res_range;                /* Resolutions range, in DPI */
-    SANE_Range   win_x_range, win_y_range; /* Window x/y ranges */
+    SANE_Range   win_x_range, win_y_range; /* Window x/y ranges, in mm */
 } devcaps_source;
 
 /* Device Capabilities
