0136898265522f5323b27b929c07b3a57f16c373
Alexander Pevzner <pzz@apevzner.com>
2020-02-16 17:24:48 +0300

    netif_notifier integrated with WS-Discovery

--- airscan-wsdd.c
+++ airscan-wsdd.c
@@ -8,11 +8,28 @@
 
 #include "airscan.h"
 
+static netif_notifier *wsdd_netif_notifier;
+
+/* Network interfaces address change notification
+ */
+static void
+wsdd_netif_notifier_callback (void *data)
+{
+    (void) data;
+    log_debug(NULL, "WSDD: netif event");
+}
+
 /* Initialize WS-Discovery
  */
 SANE_Status
 wsdd_init (void)
 {
+    wsdd_netif_notifier = netif_notifier_create(
+        wsdd_netif_notifier_callback, NULL);
+    if (wsdd_netif_notifier == NULL) {
+        return SANE_STATUS_IO_ERROR;
+    }
+
     return SANE_STATUS_GOOD;
 }
 
@@ -21,6 +38,10 @@ wsdd_init (void)
 void
 wsdd_cleanup (void)
 {
+    if (wsdd_netif_notifier != NULL) {
+        netif_notifier_free(wsdd_netif_notifier);
+        wsdd_netif_notifier = NULL;
+    }
 }
 
 /* vim:ts=8:sw=4:et
