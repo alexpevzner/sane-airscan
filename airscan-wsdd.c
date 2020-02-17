12cbacc0c00a32d94d330111815c9fb317003935
Alexander Pevzner <pzz@apevzner.com>
2020-02-17 17:05:36 +0300

    WSDD: per-interface unicast sockets management

--- airscan-wsdd.c
+++ airscan-wsdd.c
@@ -14,12 +14,12 @@
 
 #include <arpa/inet.h>
 
-/* wssd_sock represents a per-interface socket
+/* wssd_resolver represents a per-interface WSDD resolver
  */
 typedef struct {
     int          fd;      /* File descriptor */
     eloop_fdpoll *fdpoll; /* Socket fdpoll */
-} wssd_sock;
+} wssd_resolver;
 
 /* Static variables
  */
@@ -32,6 +32,86 @@ static eloop_fdpoll        *wsdd_fdpoll_ipv6;
 static char                wsdd_buf[65546];
 static struct sockaddr_in  wsdd_mcast_ipv4;
 static struct sockaddr_in6 wsdd_mcast_ipv6;
+
+/* Resolver read callback
+ */
+static void
+wssd_resolver_read_callback (int fd, void *data, ELOOP_FDPOLL_MASK mask)
+{
+    int rc;
+
+    (void) data;
+    (void) mask;
+
+    rc = read(fd, wsdd_buf, sizeof(wsdd_buf));
+    if (rc <= 0) {
+        return;
+    }
+}
+
+/* Create wssd_resolver
+ */
+static wssd_resolver*
+wssd_resolver_new (const netif_addr *addr)
+{
+    wssd_resolver *resolver = g_new0(wssd_resolver, 1);
+    int           af = addr->ipv6 ? AF_INET6 : AF_INET;
+    const char    *af_name = addr->ipv6 ? "AF_INET6" : "AF_INET";
+    int           rc;
+
+    /* Open a socket */
+    resolver->fd = socket(af, SOCK_DGRAM | SOCK_CLOEXEC | SOCK_NONBLOCK, 0);
+    if (resolver->fd < 0) {
+        log_debug(NULL, "WSDD: socket(%s): %s", af_name, strerror(errno));
+        goto FAIL;
+    }
+
+    /* Set socket options */
+    if (addr->ipv6) {
+        rc = setsockopt(resolver->fd, IPPROTO_IPV6, IPV6_MULTICAST_IF,
+                &addr->ifindex, sizeof(addr->ifindex));
+
+        if (rc < 0) {
+            log_debug(NULL, "WSDD: setsockopt(AF_INET6,IPV6_MULTICAST_IF): %s",
+                    strerror(errno));
+        }
+    } else {
+        rc = setsockopt(resolver->fd, IPPROTO_IP, IP_MULTICAST_IF,
+                &addr->ip.v4, sizeof(&addr->ip.v4));
+
+        if (rc < 0) {
+            log_debug(NULL, "WSDD: setsockopt(AF_INET,IP_MULTICAST_IF): %s",
+                    strerror(errno));
+        }
+    }
+
+    /* Setup fdpoll */
+    resolver->fdpoll = eloop_fdpoll_new(resolver->fd,
+        wssd_resolver_read_callback, NULL);
+
+    return resolver;
+
+    /* Error: cleanup and exit */
+FAIL:
+    if (resolver->fd >= 0) {
+        close(resolver->fd);
+        resolver->fd = -1;
+    }
+    return resolver;
+}
+
+/* Destroy wssd_resolver
+ */
+static void
+wssd_resolver_free (wssd_resolver *resolver)
+{
+    if (resolver->fdpoll != NULL) {
+        eloop_fdpoll_free(resolver->fdpoll);
+        close(resolver->fd);
+    }
+
+    g_free(resolver);
+}
 
 /* Read callback for multicast socket
  */
@@ -200,6 +280,17 @@ wsdd_netif_update_addresses (void) {
     for (addr = diff.added; addr != NULL; addr = addr->next) {
         int fd = addr->ipv6 ? wsdd_mcsock_ipv6 : wsdd_mcsock_ipv4;
         wsdd_mcast_update_membership(fd, addr, true);
+    }
+
+    /* Start/stop per-interface-address resolvers */
+    for (addr = diff.removed; addr != NULL; addr = addr->next) {
+        wssd_resolver_free(addr->data);
+    }
+
+    for (addr = wsdd_netif_addr_list; addr != NULL; addr = addr->next) {
+        if (addr->data == NULL) {
+            addr->data = wssd_resolver_new(addr);
+        }
     }
 }
 
