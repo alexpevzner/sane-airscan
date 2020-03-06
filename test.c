b051955717ee799cc5f58a34d8852e3bcd6fa9c5
Alexander Pevzner <pzz@apevzner.com>
2020-03-06 23:22:14 +0300

    Test updated

--- test.c
+++ test.c
@@ -36,11 +36,35 @@ check (SANE_Status status, const char *operation)
     }
 }
 
-#define TRY(func, args...)              \
-    do{                                 \
-        SANE_Status s = func(args);     \
-        check(s, #func);                \
+#define TRY(func, args...)                                              \
+    do{                                                                 \
+        printf("%s: calling\n", #func);                                 \
+        SANE_Status s = func(args);                                     \
+        printf("%s: done, status=%s\n", #func, sane_strstatus(s));      \
+        check(s, #func);                                                \
     } while(0)
+
+void
+scan_test (void)
+{
+    SANE_Status s;
+    SANE_Byte   buf[65536];
+    int         len, count = 0;
+
+    TRY(sane_start,handle);
+
+    for (;;) {
+        s = sane_read(handle, buf, sizeof(buf), &len);
+        if (s != SANE_STATUS_GOOD) {
+            break;
+        }
+
+        count += len;
+    }
+    if (count != 0) {
+        printf("%d bytes of data received\n", count);
+    }
+}
 
 int
 main (void)
@@ -58,27 +82,14 @@ main (void)
     //TRY(sane_control_option, handle, OPT_SCAN_SOURCE, SANE_ACTION_SET_VALUE, OPTVAL_SOURCE_ADF_SIMPLEX, NULL);
     TRY(sane_get_parameters, handle, &params);
     printf("image size: %dx%d\n", params.pixels_per_line, params.lines);
-    TRY(sane_start,handle);
 
-    SANE_Status s;
-    SANE_Byte   buf[65536];
-    int         len, count = 0;
+    scan_test();
+    scan_test();
 
-    for (;;) {
-        s = sane_read(handle, buf, sizeof(buf), &len);
-        if (s != SANE_STATUS_GOOD) {
-            break;
-        }
-
-        count += len;
-    }
-    if (count != 0) {
-        printf("%d bytes of data received\n", count);
-    }
+    sane_close(handle);
 
     //getchar();
 
-    sane_close(handle);
     sane_exit();
 
     return 0;
