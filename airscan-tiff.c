3776f428bbf3f68c08b2c1bb4fae1f3e103daecd
Alexander Pevzner <pzz@apevzner.com>
2020-03-28 19:30:33 +0300

    Licensing information replaced with a reference to the LICENSE file

--- airscan-tiff.c
+++ airscan-tiff.c
@@ -1,24 +1,8 @@
 /* sane - Scanner Access Now Easy.
-
-   Copyright (C) 2020 Thierry HUCHARD <thierry@ordissimo.com>
-
-   This file is part of the SANE package.
-
-   SANE is free software; you can redistribute it and/or modify it under
-   the terms of the GNU General Public License as published by the Free
-   Software Foundation; either version 3 of the License, or (at your
-   option) any later version.
-
-   SANE is distributed in the hope that it will be useful, but WITHOUT
-   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-   for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with sane; see the file COPYING.  If not, write to the Free
-   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-   This file implements a SANE backend for airscan scanners. */
+ *
+ * Copyright (C) 2020 Thierry HUCHARD <thierry@ordissimo.com>
+ * See LICENSE for license terms and conditions
+ */
 
 #include "airscan.h"
 
